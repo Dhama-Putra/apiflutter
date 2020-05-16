73455dff77acac41fd171184d7ec528ee174950c
dhmptr <dhmptr15@gmail.com>
2020-05-16 11:50:58 +0800

    first commit

--- db_penjualan.sql
+++ db_penjualan.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 21, 2020 at 01:39 PM
+-- Generation Time: May 16, 2020 at 05:48 AM
 -- Server version: 10.1.19-MariaDB
 -- PHP Version: 7.0.13
 
@@ -40,9 +40,35 @@ CREATE TABLE `tb_penjualan` (
 --
 
 INSERT INTO `tb_penjualan` (`id`, `nama`, `jenis_anjing`, `jumlah`, `harga`, `tanggal`) VALUES
-(2, 'Ngurah', 'Husky', 1, '3.200.000', '2020-04-20'),
+(2, 'Ngurah12', 'Husky', 1, '3.200.000', '2020-04-21'),
 (7, 'Dhama Putra', 'Mini Pom', 2, '2.600.000', '2020-04-21'),
 (9, 'Gusti Ngurah Dhama Putra', 'Pitbul', 1, '5.000.000', '2020-04-10');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `email` text NOT NULL,
+  `password` text NOT NULL,
+  `level` int(11) NOT NULL,
+  `nama` text NOT NULL,
+  `status` int(11) NOT NULL,
+  `createdDate` datetime NOT NULL,
+  `username` varchar(45) DEFAULT NULL,
+  `photo` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `email`, `password`, `level`, `nama`, `status`, `createdDate`, `username`, `photo`) VALUES
+(1, 'dhmptr15@gmail.com', '2440f1f9ab6d10594f63ccebed17acc4', 0, 'Dhama putra', 0, '0000-00-00 00:00:00', 'dhmptr', NULL),
+(2, 'dhmptr15@gmail.com', 'dhmptr0406', 1, 'dhama putra', 0, '2020-05-15 00:00:00', 'ngurah', NULL);
 
 --
 -- Indexes for dumped tables
@@ -55,6 +81,12 @@ ALTER TABLE `tb_penjualan`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -63,6 +95,11 @@ ALTER TABLE `tb_penjualan`
 --
 ALTER TABLE `tb_penjualan`
   MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
